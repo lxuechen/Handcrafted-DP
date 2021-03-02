5af8df6482e07d16c0931311f577fea44c1c5659
lxuechen <12689993+lxuechen@users.noreply.github.com>
2021-03-02 02:20:52 -0500

    More seeds.

--- scripts/cifar-end2end.sh
+++ scripts/cifar-end2end.sh
@@ -2,7 +2,7 @@
 
 device=0
 epochs=30
-for seed in 0 1 2 3 4; do
+for seed in 0 1 2 3 4 5 6 7; do
   CUDA_VISIBLE_DEVICES=${device} python3 cnns.py --dataset=cifar10 --batch_size=1024 --lr=1 --noise_multiplier=1.54 --seed ${seed} --epochs ${epochs} &
   ((device++))
 done

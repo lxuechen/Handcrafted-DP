6c03d61cc7275a4e9efe63cac861cd62f5d55936
lxuechen <12689993+lxuechen@users.noreply.github.com>
2021-03-02 02:20:40 -0500

    Fix record writing.

--- scripts/cifar-end2end.sh
+++ scripts/cifar-end2end.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 device=0
+epochs=30
 for seed in 0 1 2 3 4; do
-  CUDA_VISIBLE_DEVICES=${device} python3 cnns.py --dataset=cifar10 --batch_size=1024 --lr=1 --noise_multiplier=1.54 --seed ${seed} &
+  CUDA_VISIBLE_DEVICES=${device} python3 cnns.py --dataset=cifar10 --batch_size=1024 --lr=1 --noise_multiplier=1.54 --seed ${seed} --epochs ${epochs} &
   ((device++))
 done

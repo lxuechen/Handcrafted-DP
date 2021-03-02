b6351051a3f4f2f1d5d0cca9946e6be2e346aeb0
lxuechen <12689993+lxuechen@users.noreply.github.com>
2021-03-02 01:44:32 -0500

    Refactor.

--- scripts/cifar-end2end.sh
+++ scripts/cifar-end2end.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+device=0
 for seed in 0 1 2 3 4; do
-  python3 cnns.py --dataset=cifar10 --batch_size=1024 --lr=1 --noise_multiplier=1.54 --seed ${seed}
+  CUDA_VISIBLE_DEVICES=${device} python3 cnns.py --dataset=cifar10 --batch_size=1024 --lr=1 --noise_multiplier=1.54 --seed ${seed} &
+  ((device++))
 done
